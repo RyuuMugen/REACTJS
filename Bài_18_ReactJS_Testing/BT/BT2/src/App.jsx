204524caf064a87ec471c0fa4fb9207ef438a34e
RyuuMugen <thangcp8@gmail.com>
2023-06-02 21:58:54 +0700

    final

---
+++ Bài_18_ReactJS_Testing/BT/BT2/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import UserDetails from './component/User';
+
+function App() {
+  return (
+    <div className="App">
+      <UserDetails />
+    </div>
+  );
+}
+
+export default App;
